ceaacf948291ae5e40952becda8a16df8c375ff3
ellapresso <ellapresso@conbridge.co.kr>
2021-11-09 17:47:03 +0900

    [FIX]

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:ellapresso/ellapresso.github.io.git origin main
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:wowthemesnet/vuepress-theme-mediumish.git master:gh-pages
+git push -f git@github.com:ellapresso/ellapresso.github.io.git main:gh-pages
 
 cd -
