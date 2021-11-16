1590c61219c74cbbb73b0972db90199dd7cffca4
황윤지 <ellapresso@gmail.com>
2021-11-17 00:01:28 +0900

    [ADD] favicon

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,8 @@
 module.exports = {
   title: 'ellapresso',
   head: [
+    ['link', { rel : 'icon', href: '/assets/img/cat.png' }],
+    // ['link', { rel : 'icon', href: 'https://cdn.pixabay.com/photo/2014/03/25/16/24/paw-296964_1280.png' }],
     ['meta', { name: 'google-site-verification', content: 'gQyOTLNCOiZpV2eNHtevgTZ-R94aphWIg0HbgskZkgg' }],
     ['meta', { name: 'naver-site-verification', content: '193f8ae341fe542f28e9ead7749281b82e008769' }]
   ],
