8233ebf3a44df8dbbb5a4ef4600119745ce030d0
ellapresso <ellapresso@gmail.com>
2021-11-16 23:02:30 +0900

    [ADD] post

--- deploy.sh
+++ deploy.sh
@@ -25,6 +25,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f main:gh-pages
+git push -f git@github.com:ellapresso/ellapresso.github.io.git main:gh-pages
 
 cd -
