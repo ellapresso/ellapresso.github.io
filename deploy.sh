97be16d1937c164b170928a4cada616a290bcf0b
황윤지 <ellapresso@doublemedia.co.kr>
2023-02-22 10:29:34 +0900

    [ADD] post

--- deploy.sh
+++ deploy.sh
@@ -18,8 +18,8 @@ cd docs/.vuepress/dist
 # echo 'www.example.com' > CNAME
 
 git init
-git branch main
-git checkout main
+# git branch main
+# git checkout main
 git add -A
 git commit -m 'deploy'
 
